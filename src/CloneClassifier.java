c98d5f4fe1f826f887b78b1a3166ab4a1df90047
s-kento <s-kento@ist.osaka-u.ac.jp>
2016-09-23 14:49:51 +0900

    classifyGroupメソッド実装完了

--- src/CloneClassifier.java
+++ src/CloneClassifier.java
@@ -3,43 +3,55 @@ import java.util.List;
 //プロジェクト間クローンを漏れなく洗い出すクラス
 //プロジェクト間クローンセットと，プロジェクト内クローンセットのoverlap値を算出する
 public class CloneClassifier {
-	public void classifyGroup(List<CloneSet> csets){
-		for(CloneSet cset:csets){
-			if(!cset.isSingle()){//プロジェクト間のクローンならば
 
+	static double THRESHOLD = 0.7;
+
+	public void classifyGroup(List<CloneSet> csets) {
+		for (CloneSet cset1 : csets) {
+			if (!cset1.isSingle()) {// プロジェクト間のクローンならば
+				for (Clone clone1 : cset1.getCloneList()) {// 各クローンがcontainしているsingleのクローンセットを走査していく
+					for (CloneSet cset2 : csets) {
+						if (cset2.isSingle() && (clone1.getGroupId() == cset2.getGroupId())) {
+							for (Clone clone2 : cset2.getCloneList()) {
+								if ((clone1.getFileId() == clone2.getFileId()) && isContained(clone2, clone1)) {
+									if (calcContained(clone2, clone1) >= THRESHOLD) {
+										cset2.changeToNotSpec();
+									}
+								}
+							}
+						}
+					}
+				}
 			}
 		}
 	}
 
-	//containしているか判定する
-	public boolean isContained(Clone cl1, Clone cl2){
-		boolean contained=true;
-		if(cl1.getEndLine()<cl2.getStartLine() || cl1.getStartLine()>cl2.getEndLine()){
-			contained=false;
+	// containしているか判定する
+	public boolean isContained(Clone cl1, Clone cl2) {
+		boolean contained = true;
+		if (cl1.getEndLine() < cl2.getStartLine() || cl1.getStartLine() > cl2.getEndLine()) {
+			contained = false;
 		}
 		return contained;
 	}
 
-	//containの値を計算する
-	public double calcContained(Clone cl1, Clone cl2){
+	// containedの値を計算する
+	public double calcContained(Clone cl1, Clone cl2) {
 		double contained;
-		int interStart; //積集合の開始行
-		int interEnd; //積集合の終了行
+		int interStart; // 積集合の開始行
+		int interEnd; // 積集合の終了行
 
-		if(cl1.getStartLine()<=cl2.getStartLine()){
-			interStart=cl2.getStartLine();
+		if (cl1.getStartLine() <= cl2.getStartLine()) {
+			interStart = cl2.getStartLine();
+		} else {
+			interStart = cl1.getStartLine();
 		}
-		else{
-			interStart=cl1.getStartLine();
+		if (cl1.getEndLine() >= cl2.getEndLine()) {
+			interEnd = cl2.getEndLine();
+		} else {
+			interEnd = cl1.getEndLine();
 		}
-		if(cl1.getEndLine()>=cl2.getEndLine()){
-			interEnd=cl2.getEndLine();
-		}
-		else{
-			interEnd=cl1.getEndLine();
-		}
-		contained=(interEnd-interStart+1)/(cl1.getEndLine()-cl1.getStartLine()+1);
+		contained = (interEnd - interStart + 1) / (cl1.getEndLine() - cl1.getStartLine() + 1);
 		return contained;
 	}
 }
-
