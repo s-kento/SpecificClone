78bec1d6618acc4eefaf50b5c33c72983c7e13ac
s-kento <s-kento@ist.osaka-u.ac.jp>
2016-09-23 13:43:12 +0900

    overlap計算からcontained計算に変更

--- src/CloneClassifier.java
+++ src/CloneClassifier.java
@@ -1,7 +1,7 @@
 import java.util.List;
 
 //プロジェクト間クローンを漏れなく洗い出すクラス
-//プロジェクト間クローンセットと，プロジェクト内クローンセットのok値を算出する
+//プロジェクト間クローンセットと，プロジェクト内クローンセットのoverlap値を算出する
 public class CloneClassifier {
 	public void classifyGroup(List<CloneSet> csets){
 		for(CloneSet cset:csets){
@@ -11,41 +11,35 @@ public class CloneClassifier {
 		}
 	}
 
-	//overlapしているか判定する
-	public boolean isOverlap(Clone cl1, Clone cl2){
-		boolean overlap=true;
+	//containしているか判定する
+	public boolean isContained(Clone cl1, Clone cl2){
+		boolean contained=true;
 		if(cl1.getEndLine()<cl2.getStartLine() || cl1.getStartLine()>cl2.getEndLine()){
-			overlap=false;
+			contained=false;
 		}
-		return overlap;
+		return contained;
 	}
 
-	//overlapの値を計算する
-	public double calcOverlap(Clone cl1, Clone cl2){
-		double overlap;
-		int unionStart; //和集合の開始行
-		int unionEnd; //和集合の終了行
+	//containの値を計算する
+	public double calcContained(Clone cl1, Clone cl2){
+		double contained;
 		int interStart; //積集合の開始行
 		int interEnd; //積集合の終了行
 
 		if(cl1.getStartLine()<=cl2.getStartLine()){
-			unionStart=cl1.getStartLine();
 			interStart=cl2.getStartLine();
 		}
 		else{
-			unionStart=cl2.getStartLine();
 			interStart=cl1.getStartLine();
 		}
 		if(cl1.getEndLine()>=cl2.getEndLine()){
-			unionEnd=cl1.getEndLine();
 			interEnd=cl2.getEndLine();
 		}
 		else{
-			unionEnd=cl2.getEndLine();
 			interEnd=cl1.getEndLine();
 		}
-		overlap=(interEnd-interStart+1)/(unionEnd-unionStart+1);
-		return overlap;
+		contained=(interEnd-interStart+1)/(cl1.getEndLine()-cl1.getStartLine()+1);
+		return contained;
 	}
 }
 
