4a7ac7ed18a6ed76c7e3d383c7127c2b43bdfbd6
s-kento <s-kento@ist.osaka-u.ac.jp>
2016-09-23 14:51:30 +0900

    classifyGroup実行の処理を追加

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		clonesets = new CloneReader().fileRead(args[0]);
+		CloneClassifier ccf = new CloneClassifier();
+		ccf.classifyGroup(clonesets);
 		for (CloneSet cset : clonesets) {
-			System.out.println(cset.getSetNum()+"番目のクローンセット");
-			if(cset.isSingle())
-				System.out.println("同一グループです．");
-			else
+			//System.out.println(cset.getSetNum()+"番目のクローンセット");
+			if(cset.isSpec()){
+				//System.out.println("グループ固有です．");
+			}
+			else{
 				System.out.println("グループ間です");
+
 			for (Clone clone : cset.clonelist) {
 				System.out.print(clone.getGroupId()+".");
 				System.out.print(clone.getFileId()+"\t");
 				System.out.print(clone.getStartLine()+"\t");
 				System.out.println(clone.getEndLine()+"\t");
+			}
 			}
 		}
 	}
