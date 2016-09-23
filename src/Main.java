082a2cd2f72248491f3dc80106d8dc0894a3064c
s-kento <s-kento@ist.osaka-u.ac.jp>
2016-09-23 16:31:17 +0900

    ResultFileControllerの実行処理を追加

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,27 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 	static List<CloneSet> clonesets = new ArrayList<CloneSet>();
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		clonesets = new CloneReader().fileRead(args[0]);
 		CloneClassifier ccf = new CloneClassifier();
 		ccf.classifyGroup(clonesets);
+		ResultFileControler rfc = new ResultFileControler();
+		rfc.fileRead(args[0]);
+		rfc.fileWrite(clonesets, args[1]);
 		for (CloneSet cset : clonesets) {
-			//System.out.println(cset.getSetNum()+"番目のクローンセット");
-			if(cset.isSpec()){
-				//System.out.println("グループ固有です．");
-			}
-			else{
+			if (!cset.isSpec()) {
+				System.out.println(cset.getSetNum()+"番目のクローンセット");
 				System.out.println("グループ間です");
-
-			for (Clone clone : cset.clonelist) {
-				System.out.print(clone.getGroupId()+".");
-				System.out.print(clone.getFileId()+"\t");
-				System.out.print(clone.getStartLine()+"\t");
-				System.out.println(clone.getEndLine()+"\t");
-			}
+				for (Clone clone : cset.clonelist) {
+					System.out.print(clone.getGroupId() + ".");
+					System.out.print(clone.getFileId() + "\t");
+					System.out.print(clone.getStartLine() + "\t");
+					System.out.println(clone.getEndLine() + "\t");
+				}
 			}
 		}
 	}
